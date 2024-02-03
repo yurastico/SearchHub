bb80da50bfc53410979d796e75d53fc65845419a
yurastico <cunha.ysc@gmail.com>
2024-02-03 10:45:27 -0300

    using useState for the first time

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
+
+
+
 function Home() { 
+    const [name,setName] = useState<String>("")
+    function handleName() { 
+        setName("tu tu ruuuu")
+    }
+
     return ( 
+        <div>
         <h1>YURASTICO</h1>
+        <span>{name}</span>
+        <button type="button" onClick={handleName}>Click me!</button>
+        </div>
     )
 }
 
